cfd719f73c4f661198b683c5bae5be02440a2a2d
StoplightLoosejaw <57860542+StoplightLoosejaw@users.noreply.github.com>
2020-02-02 21:21:58 +0300

    Update settings.py

--- qualitypics/settings.py
+++ qualitypics/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+        
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
