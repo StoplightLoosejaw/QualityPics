4595510b2ab49f37a63fb16c9f1f42e5d3483018
StoplightLoosejaw <57860542+StoplightLoosejaw@users.noreply.github.com>
2020-02-02 21:50:33 +0300

    Update settings.py

--- qualitypics/settings.py
+++ qualitypics/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage.'
