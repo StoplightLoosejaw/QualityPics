671387e5079fec1cf26b2cfa22238632f2c9c183
StoplightLoosejaw <57860542+StoplightLoosejaw@users.noreply.github.com>
2020-02-03 22:45:02 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,8 +3,12 @@ from .models import Pic
 from . import serializers
 from rest_framework import generics, status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
 import random
+from .forms import LoginForm
 from braces import views
+from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -13,6 +17,28 @@ def Home(request):
     maxval = len(queryset)
     return render(request, 'home.html', { 'maxval': maxval})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+    
 class RandomPicView(generics.RetrieveAPIView):
     queryset = Pic.objects.all()
     serializer_class = serializers.PicSerializer
